c70569b67ac4e5da93b11d2d9aa08f410cbb4b49
vikash sharma <vikashtiwari9893@gmail.com>
2025-01-01 23:01:22 +0530

    Check string is empty or not

---
+++ EmptyString/index.js
@@ -0,0 +1,53 @@
+
+
+
+// check string is empty or not
+const str =""
+
+if(str.length==0){
+    console.log("String is empty")
+}
+else{
+    console.log("String is  not empty")
+}
+
+
+// check string is empty or whitespace
+
+const str1 = "   "
+
+if(str1.trim()== ""){
+    console.log("string is empty and whitespace")
+}
+
+else{
+    console.log("string is not empty")
+}
+
+
+// check by simple methods
+
+const str2 = ""
+
+if(str2  == ""){
+    console.log("string is empty ")
+}
+
+else{
+    console.log("string is not empty")
+}
+
+
+
+
+// check by taking input
+const str3 = prompt("Enter any string")
+
+if(str3  == ""){
+    console.log("string is empty ")
+}
+
+else{
+    console.log("string is not empty")
+}
+
